001257b8523923faacee4849c3e8321a6f992523
Gamalliel Sharon <you@example.com>
2020-08-04 12:36:42 +0700

    Menambahkan tugas ke 11 javascript

---
+++ tugas11/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserInfo from './UserInfo'  
+
+function App() {
+  return (
+    <div className="App">
+      <UserInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ tugas11/src/UserInfo.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+
+class NamaBuah extends React.Component{
+    render(){
+        return <p>{this.props.nama}</p>
+    }
+}
+class HargaBuah extends React.Component{
+    render(){
+        return <p>{this.props.harga}</p>
+    }
+}
+class BeratBuah extends React.Component{
+    render(){
+        return <p>{this.props.berat}</p>
+    }
+}
+
+let dataHargaBuah = [
+    {nama: "Semangka", harga: 10000, berat: 1000},
+    {nama: "Anggur", harga: 40000, berat: 500},
+    {nama: "Strawberry", harga: 30000, berat: 400},
+    {nama: "Jeruk", harga: 30000, berat: 1000},
+    {nama: "Mangga", harga: 30000, berat: 500}
+  ]
+
+
+  class UserInfo extends React.Component {
+    render() {
+      return (
+        <>
+          {dataHargaBuah.map(el=> {
+            return (
+              <table style={{padding:"20px", margin:"20px auto"}}>
+                  <tr>
+                      <th>
+                          Nama
+                      </th>
+                      <th>
+                          Harga
+                      </th>
+                      <th>
+                          Berat
+                      </th>
+                  </tr>
+                  <tr>
+                      <td>
+                          <NamaBuah nama={el.nama}/>
+                      </td>
+                      <td>
+                          <HargaBuah harga={el.harga}/>
+                      </td>
+                      <td>
+                          <BeratBuah berat={el.berat}/>
+                      </td>
+                  </tr>
+                  
+              </table>
+            )
+          })}
+        </>
+      )
+    }
+  }
+  
+  export default UserInfo
