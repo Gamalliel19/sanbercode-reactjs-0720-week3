36bc74e411be8b3834175d30682a0a8a9a66d82b
Gamalliel Sharon <you@example.com>
2020-08-07 09:24:20 +0700

    Menambahkan tugas hari ke 14 javascript

--- tugas11/src/App.js
+++ tugas11/src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 import DaftarBuah from './tugas11/DaftarBuah'
 import Timer from './tugas12/Timer'
 import Lists from './tugas13/lists'
+import Tugas14 from './tugas14/tugas14'
 
 function App() {
   return (
     <div className="App">
       <h1>Tabel Harga Buah</h1>
-      <Timer/>
-      <Lists/>
-      <DaftarBuah/>
+      <Tugas14 />
     </div>
   );
 }
