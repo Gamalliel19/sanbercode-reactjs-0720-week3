cc4c2638bea2085a364e0a735d514dd8e97606af
Gamalliel Sharon <you@example.com>
2020-08-08 11:36:05 +0700

    Menambahkan tugas ke 15 Javascript

--- tugas11/src/App.js
+++ tugas11/src/App.js
@@ -4,15 +4,15 @@ import DaftarBuah from './tugas11/DaftarBuah'
 import Timer from './tugas12/Timer'
 import Lists from './tugas13/lists'
 import Tugas14 from './tugas14/tugas14'
+import {BrowserRouter as Router} from "react-router-dom";
+import Routes from "./tugas15/Routes";
 
 function App() {
   return (
     <div className="App">
-      <h1>Tabel Harga Buah</h1>
-      <Tugas14 />
-      <Timer/>
-      <DaftarBuah/>
-      <Lists/>
+      <Router>
+        <Routes/>
+      </Router>
     </div>
   );
 }
